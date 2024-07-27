86f61e8259154f9fe854ba5964fb71cb89e0a0cc
WooSeong Chung <jungyh0528@gmail.com>
2024-07-27 15:52:06 +0900

    feat: init firebase

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+require("dotenv").config();
+
+const nextConfig = {
+  env: {
+    FIREBASE_APIKEY: process.env.FIREBASE_APIKEY,
+    FIREBASE_AUTH_DOMANIN: process.env.FIREBASE_AUTH_DOMANIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+  },
+};
 
 export default nextConfig;
