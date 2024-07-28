7da5a17ac670454b62b4b82732fedef4547f9393
WooSeong Chung <jungyh0528@gmail.com>
2024-07-28 13:54:07 +0900

    feat: add sample firestore read create code

--- frontend/app/lib/firebase.ts
+++ frontend/app/lib/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore, getDocs, collection, addDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,3 +17,24 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function createDocFirestore(key: string, data: any) {
+  try {
+    const docRef = await addDoc(collection(db, key), data);
+    console.log("Success!: ", docRef.id);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export async function getDocsFirestore(key: string) {
+  try {
+    const querySnapshot = await getDocs(collection(db, key));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
