3c9e3e24f1d10c038348ff50e46324110b05dfa6
WooSeong Chung <jungyh0528@gmail.com>
2024-07-28 11:15:44 +0900

    feat: init firebase and refactor frontends

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-require("dotenv").config();
+import { config } from "dotenv";
+config();
 
 const nextConfig = {
+  reactStrictMode: false,
   env: {
     FIREBASE_APIKEY: process.env.FIREBASE_APIKEY,
-    FIREBASE_AUTH_DOMANIN: process.env.FIREBASE_AUTH_DOMANIN,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
     FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
     FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
