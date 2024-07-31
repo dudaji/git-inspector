86be5bb44e0a42c862bb6098f42a783770a418a7
DONGHYUN YOUN <kade@dudaji.com>
2024-07-31 12:56:20 +0900

    working machine type crawler

--- cloud-functions/functions/gcp_cloud_scraper/gcp_spider.py
+++ cloud-functions/functions/gcp_cloud_scraper/gcp_spider.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import time
+
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 import scrapy
 from scrapy.crawler import CrawlerProcess
@@ -11,7 +12,10 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException
+from selenium.common.exceptions import (
+    StaleElementReferenceException,
+    NoSuchElementException,
+)
 from selenium.webdriver.common.action_chains import ActionChains
 
 # from google.cloud import firestore
@@ -25,9 +29,10 @@ import logging
 
 # from functions.clouds._firestore import save_to_firestore
 
+
 class GCPSpider(scrapy.Spider):
     name = "gcp_spider"
-    
+
     def start_requests(self):
         region_urls = {
             "asia-northeast3": "https://cloud.google.com/products/calculator?hl=ko&region=asia-northeast3&dl=CiQ1ZGQyZTU1OS1lNzlmLTRkODAtODE3NS04YTBlOTkyMmMyNzAQCBokOEE5NDZFQ0QtMDYzMC00MDUzLThGRDMtOTIyNTY0QTNDNTE2",
@@ -36,69 +41,145 @@ class GCPSpider(scrapy.Spider):
 
         # 하나의 WebDriver 인스턴스 생성
         chrome_options = Options()
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
 
-
         for region, url in region_urls.items():
-            region_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+            region_driver = webdriver.Chrome(
+                service=Service(ChromeDriverManager().install()),
+                options=chrome_options,
+            )
             region_driver.get(url)
             WebDriverWait(region_driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, '//ul[@aria-label="Series"]//li[@role="option"]'))
+                EC.presence_of_element_located(
+                    (By.XPATH, '//ul[@aria-label="Series"]//li[@role="option"]')
+                )
             )
             self.parse_machine_type(region_driver, region)
             region_driver.quit()
 
     def parse_machine_type(self, driver, region):
-        series_elements = driver.find_elements(By.XPATH, '//ul[@aria-label="Series"]//li[@role="option"]')
+        series_elements = driver.find_elements(
+            By.XPATH, '//ul[@aria-label="Series"]//li[@role="option"]'
+        )
         for element in series_elements:
             series_value = self.get_element_attribute(element, "data-value")
             if series_value:
                 # print(f"Found series with value: {series_value}")
+                # have to click series with loop
                 self.parse_machine_name_spec(driver, region, series_value)
 
-    
+    # have to click machine type and parse all data (final)
     def parse_machine_name_spec(self, driver, region, series_value):
-        machine_elements = driver.find_elements(By.XPATH, '//ul[@aria-label="Machine type"]/li[@data-value]')
-        
+        try:
+            cookie_consent_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//button[text()="나중에"]'))
+            )
+            cookie_consent_button.click()
+        except Exception as e:
+            print("쿠키 동의 창을 찾을 수 없습니다:", e)
+        dropdown_button_xpath = (
+            '//div[@aria-controls="i27" and @aria-haspopup="listbox"]'
+        )
+        dropdown_area = driver.find_element(By.XPATH, dropdown_button_xpath)
+        dropdown_area.click()
+        time.sleep(2)  # 2초 대기
+        # li element 클릭
+        machine_elements = driver.find_elements(
+            By.XPATH, '//ul[@aria-label="Machine type"]/li[@data-value]'
+        )
+        # 드롭다운 버튼을 클릭하기 위해 Actions 사용
         for element in machine_elements:
             # element = div, spans
-            machine_type_value = self.get_element_attribute(element, "data-value")
-            print(f"Found machine type value : machine name: {machine_type_value}, region : {region}, machine-series: {series_value}")
-            try:
-                cookie_consent_button = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, '//button[text()="나중에"]'))
-                )
-                cookie_consent_button.click()
-            except Exception as e:
-                print("쿠키 동의 창을 찾을 수 없습니다:", e)
-            # 드롭다운 버튼을 클릭하기 위해 Actions 사용
-            dropdown_button_xpath = '//div[@aria-controls="i27" and @aria-haspopup="listbox"]'
-            dropdown_area = driver.find_element(By.XPATH, dropdown_button_xpath)
-            dropdown_area.click()
-            time.sleep(2)  # 2초 대기
-            # li element 클릭
-            
-            
-            # 머신 타입 정보가 포함된 요소를 기다림
+            machine_type_value = self.get_element_attribute(
+                element, "data-value"
+            )
+            # print(
+            #     f"Found machine type value : machine name: {machine_type_value}, region : {region}, machine-series: {series_value}"
+            # )
+            # 해당 요소를 클릭하여 선택
+            if machine_type_value == "custom":
+                print(f"skip machine type custom")
+                continue
+            actions = ActionChains(driver)
+            actions.move_to_element(element).click().perform()
+            # print("clicked machine element li")
+
+            # 선택한 machine_type이 detail_element에 반영될 때까지 대기
+            detail_element_xpath = f"//div[contains(@class, 'VVW32d')]//div[contains(text(), '{machine_type_value}')]"
             detail_element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'coMZGe')]"))
+                EC.text_to_be_present_in_element(
+                    (By.XPATH, detail_element_xpath), machine_type_value
+                )
+            )
+            # print(f"Selected machine type: {machine_type_value}")
+
+            # vCPUs와 RAM 정보가 포함된 요소 기다림
+            vcpu_ram_xpath = "//div[contains(@class, 'VVW32d')]//div[contains(text(), 'vCPUs')]"
+            vcpu_ram_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, vcpu_ram_xpath))
             )
             # vCPUs와 RAM 정보 추출
-            spec_text = detail_element.find_element(By.XPATH, ".//div[contains(text(), 'vCPUs')]").text
+            spec_text = vcpu_ram_element.text
+            # print(f"spec_text : {spec_text}")
 
             # vCPUs와 RAM 값 파싱
-            vcpus = spec_text.split(',')[0].split(':')[1].strip()
-            ram = spec_text.split(',')[1].split(':')[1].replace("GB", "").strip()
+            vcpus = spec_text.split(",")[0].split(":")[1].strip()
+            ram = (
+                spec_text.split(",")[1].split(":")[1].replace("GB", "").strip()
+            )
 
-            if vcpus.replace('.', '', 1).isdigit() and ram.replace('.', '', 1).isdigit():
-                print(f"Machine type: {machine_type_value}")
-                print(f"vCPUs: {vcpus}, RAM: {ram}")
-                print(f"Region: {region}, Series: {series_value}")
-            
-    
+            if (
+                vcpus.replace(".", "", 1).isdigit()
+                and ram.replace(".", "", 1).isdigit()
+            ):
+                # print(f"Machine type: {machine_type_value}")
+                # print(f"vCPUs: {vcpus}, RAM: {ram}")
+                # print(f"Region: {region}, Series: {series_value}")
+
+                # 월별 가격 정보가 포함된 요소 기다림
+                price_xpath = "//div[contains(@class, 'egBpsb')]/span[contains(@class, 'D0aEmf')]"
+                price_element = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, price_xpath))
+                )
+            # print(f"price_element info : {price_element}")
+
+            # 월별 가격 정보 추출
+            price_text = price_element.text
+            # print(f"price_text : {price_text}")
+
+            # 월별 가격을 시간별 가격으로 환산
+            # 쉼표를 제거하고 월별 가격을 시간별 가격으로 환산
+            monthly_price = float(price_text.replace("$", "").replace(",", ""))
+            hourly_price = monthly_price / (
+                30 * 24
+            )  # 한 달을 30일로 가정하고 시간당 가격을 계산
+            # print(f"Hourly price: ${hourly_price:.4f}")
+            # CloudCost 모델 생성
+            cloud_cost = CloudCost(
+                vendor="GCP",
+                name=machine_type_value,
+                region=region,
+                cpu=vcpus,
+                ram=ram,
+                cost_per_hour=hourly_price,
+                extraction_date=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            )
+            print(cloud_cost)
+            # time.sleep(100)
+            # 드롭다운을 다시 클릭하여 열기
+            actions.move_to_element(dropdown_area).click().perform()
+
+            # 드롭다운 메뉴 항목 기다리기
+            WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located(
+                    (By.XPATH, '//ul[@aria-label="Machine type"]')
+                )
+            )
+            time.sleep(1)  # 2초 대기
+
     def get_element_attribute(self, element, attribute):
         try:
             return element.get_attribute(attribute)
@@ -111,13 +192,16 @@ class GCPSpider(scrapy.Spider):
         except StaleElementReferenceException:
             return ""
 
+
 if __name__ == "__main__":
     # Scrapy logging 설정을 최소화
-    logging.getLogger('scrapy').setLevel(logging.ERROR)
+    logging.getLogger("scrapy").setLevel(logging.ERROR)
 
-    process = CrawlerProcess(settings={
-        'LOG_LEVEL': 'ERROR',  # 전체 Scrapy 로그 레벨 설정
-    })
+    process = CrawlerProcess(
+        settings={
+            "LOG_LEVEL": "ERROR",  # 전체 Scrapy 로그 레벨 설정
+        }
+    )
 
     process.crawl(GCPSpider)
     process.start()
