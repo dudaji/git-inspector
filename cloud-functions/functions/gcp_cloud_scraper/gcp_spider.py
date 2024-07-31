73228b69dcd5efb7ced215a8f6f10ca66544b84c
DONGHYUN YOUN <kade@dudaji.com>
2024-07-31 10:28:09 +0900

    open dropdwon success

--- cloud-functions/functions/gcp_cloud_scraper/gcp_spider.py
+++ cloud-functions/functions/gcp_cloud_scraper/gcp_spider.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import time
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 import scrapy
 from scrapy.crawler import CrawlerProcess
@@ -12,6 +12,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
 
 # from google.cloud import firestore
 from datetime import datetime
@@ -35,7 +36,7 @@ class GCPSpider(scrapy.Spider):
 
         # 하나의 WebDriver 인스턴스 생성
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
@@ -58,19 +59,45 @@ class GCPSpider(scrapy.Spider):
                 # print(f"Found series with value: {series_value}")
                 self.parse_machine_name_spec(driver, region, series_value)
 
+    
     def parse_machine_name_spec(self, driver, region, series_value):
         machine_elements = driver.find_elements(By.XPATH, '//ul[@aria-label="Machine type"]/li[@data-value]')
+        
         for element in machine_elements:
+            # element = div, spans
             machine_type_value = self.get_element_attribute(element, "data-value")
-            print(f"Found machine type value : {machine_type_value}, {region}, {series_value}")
-            # 머신 타입 선택
-            # "custom" 타입은 스킵
-            if machine_type_value == "custom":
-                print(f"Skipping custom type: {machine_type_value}")
-                continue
-            # 선택에서 막힘
+            print(f"Found machine type value : machine name: {machine_type_value}, region : {region}, machine-series: {series_value}")
+            try:
+                cookie_consent_button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, '//button[text()="나중에"]'))
+                )
+                cookie_consent_button.click()
+            except Exception as e:
+                print("쿠키 동의 창을 찾을 수 없습니다:", e)
+            # 드롭다운 버튼을 클릭하기 위해 Actions 사용
+            dropdown_button_xpath = '//div[@aria-controls="i27" and @aria-haspopup="listbox"]'
+            dropdown_area = driver.find_element(By.XPATH, dropdown_button_xpath)
+            dropdown_area.click()
+            time.sleep(2)  # 2초 대기
+            # li element 클릭
+            
+            
+            # 머신 타입 정보가 포함된 요소를 기다림
+            detail_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'coMZGe')]"))
+            )
+            # vCPUs와 RAM 정보 추출
+            spec_text = detail_element.find_element(By.XPATH, ".//div[contains(text(), 'vCPUs')]").text
 
+            # vCPUs와 RAM 값 파싱
+            vcpus = spec_text.split(',')[0].split(':')[1].strip()
+            ram = spec_text.split(',')[1].split(':')[1].replace("GB", "").strip()
 
+            if vcpus.replace('.', '', 1).isdigit() and ram.replace('.', '', 1).isdigit():
+                print(f"Machine type: {machine_type_value}")
+                print(f"vCPUs: {vcpus}, RAM: {ram}")
+                print(f"Region: {region}, Series: {series_value}")
+            
     
     def get_element_attribute(self, element, attribute):
         try:
